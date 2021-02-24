c37efd6913ab50f761517269e65a093418313c43
michaeldaineka <michaeldaineka@gmail.com>
2021-02-24 10:00:54 +0300

    aowkadw

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,9 @@
 const hostApi =
   process.env.NODE_ENV === "development"
     ? "http://localhost"
-    : "https://flatlogic-ecommerce-backend.herokuapp.com:41521";
+    : "https://flatlogic-ecommerce-backend.herokuapp.com";
 const portApi =
-  process.env.NODE_ENV === "development" ? "8080" : process.env.PORT;
+  process.env.NODE_ENV === "development" ? "8080" : '41521';
 const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/api`;
 
 export default {
