81aa8e9f739ea34380dc7123ea9d20e297b5a7f7
Vlad <excelent91@mail.ru>
2021-04-06 18:13:16 +0300

    added loader

--- components/admin/Layout/Layout.js
+++ components/admin/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from 'next/router'
+import Loader from "components/admin/Loader";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Hammer from "rc-hammerjs";
 import Header from "../Header";
@@ -63,8 +64,9 @@ class Layout extends React.Component {
   }
 
   render() {
-
+    console.log(this.props.currentUser, this.props.loadingInit)
     if (!this.props.currentUser) {
+      if (this.props.loadingInit) return <Loader />
       return (
         <>
           <HeaderComp />
